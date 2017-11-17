1ad6f1096d6db8063f99421e33f2c27a301f40a0
Christen Garcia <30610163+christengarcia@users.noreply.github.com>
2017-11-17 22:58:46 +1100

    First commit

---
+++ BuildingBlocks_002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace BuildingBlocks_002
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myString = "Hello World";
+            Console.WriteLine(myString);
+            int myFirstValue = 23;
+            double mySecondValue = 45.79;
+            double sum = myFirstValue + mySecondValue;
+            Console.WriteLine(sum.ToString());
+        }
+    }
+}
